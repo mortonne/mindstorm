0b97bb3751e1bea9012198af7dc310b9d77c841e
Neal Morton <mortonne@gmail.com>
2019-08-28 19:49:41 -0500

    fix package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mindstorm-mortonne",
+    name="mindstorm",
     version="0.0.1",
     author="Neal Morton",
     author_email="mortonne@gmail.com",
